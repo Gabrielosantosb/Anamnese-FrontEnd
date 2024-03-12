df00fedc0cc4956c91d67497a9ca5b5f35a68e78
Gabriel Santos <gabrielosantosb@outlook.com>
2024-03-12 13:58:52 -0300

    refatorando pastas

---
+++ src/models/interfaces/pacients/event/editPacient.ts
@@ -0,0 +1,6 @@
+export interface EditPacientAction{
+  action: string
+  id?: string
+  categoryName?: string
+
+}
---
+++ src/models/interfaces/pacients/get-pacient-service.service.ts
@@ -0,0 +1,13 @@
+export interface GetPacientsResponse
+{
+  id: string,
+  username: string,
+  email: string,
+  adress: string,
+  uf: string,
+  phone: string,
+  birth: string,
+  gender: string,
+  profession: string,
+  doctorId: number
+}
