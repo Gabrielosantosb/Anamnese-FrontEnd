84ee4493ceee2d2857436f1800a7f3c5f18c14ce
Gabriel Santos <gabrielosantosb@outlook.com>
2024-03-27 17:26:25 -0300

    copiando link

--- src/app/shared/components/toolbar-navigation/toolbar-navigation.component.ts
+++ src/app/shared/components/toolbar-navigation/toolbar-navigation.component.ts
@@ -3,6 +3,9 @@ import {ConfirmationModal} from "../../../services/confirmation/confirmation-ser
 import {DialogService} from "primeng/dynamicdialog";
 import {ClipboardService} from "ngx-clipboard";
 import {NzMessageService} from "ng-zorro-antd/message";
+import {ToastMessage} from "../../../services/toast-message/toast-message";
+import {CookieService} from "ngx-cookie-service";
+import {environments} from "../../../../environments/environments";
 
 @Component({
   selector: 'app-toolbar-navigation',
@@ -11,18 +14,20 @@ import {NzMessageService} from "ng-zorro-antd/message";
   providers: [ConfirmationModal,]
 })
 export class ToolbarNavigationComponent {
-
+  private readonly USER_AUTH = environments.COOKIES_VALUE.user_auth
+  private token = this.cookie.get(this.USER_AUTH)
   constructor(private confirmationModal: ConfirmationModal,
-              private dialogService: DialogService,
+              private cookie : CookieService,
               private clipboardService: ClipboardService,
-
+              private toastMessage: ToastMessage,
               private messageService: NzMessageService) {
   }
 
   getIntegrationLink():void{
-    const url = 'teste123'
+    const url = `http://localhost:53532/${this.token}`
     this.clipboardService.copyFromContent(url)
-    this.messageService.info('Link para anamnese copiado com sucesso!')
+    this.toastMessage.SuccessMessage('Link para anamnese copiado com sucesso!')
+    // this.messageService.info('Link para anamnese copiado com sucesso!')
   }
   navigateAnamneseForm():void{
     this.confirmationModal.confirmNavigatePacientForm('Link para preencher Anamnese')
