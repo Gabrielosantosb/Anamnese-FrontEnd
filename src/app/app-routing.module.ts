d4fb3b2b81b450ddcd68662cac0a6c57e04cb05e
Gabriel Santos <gabrielosantosb@outlook.com>
2024-04-02 13:59:17 -0300

    Criando tela NotFound

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,6 +3,7 @@ import {PreloadAllModules, RouterModule, Routes} from '@angular/router';
 import {HomeComponent} from "./modules/home/home.component";
 import {DashboardHomeComponent} from "./modules/dashboard/page/dashboard-home/dashboard-home.component";
 import {AuthGuardService} from "./guards/auth-guard.service";
+import {NotfoundComponent} from "./comon-pages/notfound/notfound.component";
 
 const routes: Routes = [
   {
@@ -33,7 +34,13 @@ const routes: Routes = [
     loadChildren: ()=> import('./modules/pacients/pacients.module')
       .then((m)=> m.PacientsModule),
     canActivate:[AuthGuardService]
-  }
+  },
+  {
+    path: "404",
+    loadChildren:() => import('../app/comon-pages/notfound/not-found.module')
+      .then((m) => m.NotFoundModule),
+  },
+  { path: "**", component: NotfoundComponent },
 ];
 
 @NgModule({
